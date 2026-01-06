6edd6bbb14a8a5ec7ffddd4f185216599909490d
Ruwan <ruwanliyanagegalle1994@gmail.com>
2026-01-06 20:53:38 +0530

    Add docker commands

--- init.sh
+++ init.sh
@@ -1,5 +1,19 @@
 #!bash
 
-echo "Enter Your Name"
-read NAME
-echo "Hello: $NAME"
+VERSION="1.0.0"
+MEMORY="1024m"
+REPOSITORY="org_comp"
+PROJECT="java_with_alpine"
+
+set -e
+
+docker build -t ${PROJECT}:latest .
+docker tag ${PROJECT}:latest ${REPOSITORY}/${PROJECT}:${VERSION}
+docker run -d \
+--memory=${MEMORY} \
+--name=${PROJECT} \
+--cpus="4"
+--p 8080:8080
+java_with_alpine:${VERSION}
+docker push ${REPOSITORY}/${PROJECT}:${VERSION}
+echo "Done successfully"
